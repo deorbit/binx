d27502b18f2d472d98b54726ad02763d204951aa
Adam Hollidge <adam@anh.io>
2018-01-07 22:59:35 -0500

    Playing around with central state management and untangling user actions from rendered view.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strconv"
+	"sync"
 
 	"github.com/mattn/go-runewidth"
 
@@ -19,6 +19,16 @@ const (
 	NormalMode = iota
 	SeekInputMode
 	PatternInputMode
+)
+
+// Actions
+const (
+	BinxResize         = "BINX_RESIZE"
+	BinxEscape         = "BINX_ESCAPE"
+	BinxKeyUp          = "BINX_KEYUP"
+	BinxKeyDown        = "BINX_KEYDOWN"
+	BinxSetScreenStyle = "BINX_SETSTYLE"
+	BinxToggleSeekMode = "BINX_TOGGLE_SEEKMODE"
 )
 
 // emitStr prepares a string for render. Will appear after Screen.Show()
@@ -52,13 +62,75 @@ type binxConfig struct {
 	highlightLength int64
 }
 
+type AppState struct {
+	mutex           sync.Mutex
+	filename        string
+	dat             []byte
+	screen          tcell.Screen
+	byteVisWidth    int
+	byteVisHeight   int
+	statStyle       tcell.Style
+	byteStyle       tcell.Style
+	alertStyle      tcell.Style
+	startByte       int64
+	mode            int
+	userInput       string
+	highlightPos    int64
+	highlightLength int64
+	lastAction      string
+	status          string
+}
+
+type Action struct {
+	name  string
+	value interface{}
+}
+
+type Store struct {
+	Dispatcher chan Action
+	reducer    func(Action) *AppState
+	state      *AppState
+}
+
+func CreateStore(rootReducer func(Action) *AppState) Store {
+	return Store{reducer: rootReducer, Dispatcher: make(chan Action, 20)}
+}
+
+// Reduce waits for events on the dispatcher channel then runs them
+// through the user-defined reducer to update app state.
+func (s *Store) Reduce() {
+	for {
+		select {
+		case action := <-s.Dispatcher:
+			s.reducer(action)
+		}
+	}
+}
+
+// HandleTcellEvent translates tcell UI events into Actions
+// that can be consumed by our state reducer.
+func HandleTcellEvent(store Store, ev tcell.Event) {
+	switch ev := ev.(type) {
+	case *tcell.EventResize:
+		store.Dispatcher <- Action{name: BinxResize, value: ev}
+	case *tcell.EventKey:
+		if ev.Key() == tcell.KeyEscape {
+			store.Dispatcher <- Action{name: BinxEscape, value: ev}
+		} else if ev.Key() == tcell.KeyDown {
+			store.Dispatcher <- Action{name: BinxKeyDown, value: ev}
+		} else if ev.Key() == tcell.KeyUp {
+			store.Dispatcher <- Action{name: BinxKeyUp, value: ev}
+		}
+	}
+}
+
 // emitStatBar renders the status bar
-func emitStatBar(conf binxConfig) {
-	w, h := conf.screen.Size()
+func emitStatBar(state *AppState) {
+	w, h := state.screen.Size()
 	numVisibleBytes := w * h
-	emitStr(conf.screen,
-		0, h-1, conf.statStyle,
-		fmt.Sprintf("--%d--%d--%d chars--", conf.startByte, conf.startByte+int64(numVisibleBytes), numVisibleBytes))
+	emitStr(state.screen,
+		0, h-1, state.statStyle,
+		fmt.Sprintf("--%d--%d--%d chars--Last Action: %s--Status: %s", state.startByte, state.startByte+int64(numVisibleBytes), numVisibleBytes, state.lastAction, state.status))
 }
 
 // findPattern searches buf for a byte pattern specified by the hex
@@ -74,6 +146,62 @@ func findBytePattern(p string, buf []byte) (int64, error) {
 	return int64(loc), nil
 }
 
+// rootReducer is a closure around the state object. It returns
+// a function that takes an Action. That is the function we pass
+// to CreateStore(). rootReducer is run by Reduce() when an event appears
+// on the Dispatcher channel. We don't bother with a deep copy of the state
+// object; we're just careful with our mutex.
+func rootReducer(state *AppState) func(Action) *AppState {
+	return func(action Action) *AppState {
+		state.mutex.Lock()
+		state.lastAction = action.name
+		switch action.name {
+		case BinxKeyUp:
+			w := state.byteVisWidth
+			state.startByte -= int64(w)
+			if state.startByte < 0 {
+				state.startByte = 0
+			}
+			// state.screen.Sync()
+		case BinxKeyDown:
+			state.startByte += int64(state.byteVisWidth)
+			// state.screen.Sync()
+			// state.screen.Show()
+		case BinxResize:
+			_, h := state.screen.Size()
+			state.byteVisHeight = h - 1
+		// 	state.screen.Sync()
+		case BinxEscape:
+			state.screen.Fini()
+			os.Exit(0)
+		case BinxSetScreenStyle:
+			state.screen.SetStyle(action.value.(tcell.Style))
+		default:
+			break
+		}
+		state.mutex.Unlock()
+		return state
+	}
+}
+
+func render(state *AppState) {
+	state.screen.Show()
+	state.mutex.Lock()
+	w := state.byteVisWidth
+	h := state.byteVisHeight
+	numVisibleBytes := w * h
+	if numVisibleBytes < 0 {
+		numVisibleBytes = 0
+	}
+	for i, b := range state.dat[state.startByte : state.startByte+int64(numVisibleBytes)] {
+		state.byteStyle = state.byteStyle.Foreground(tcell.Color(b))
+		state.screen.SetContent(i%w, i/w, tcell.RuneBoard, nil, state.byteStyle)
+	}
+	emitStatBar(state)
+	state.mutex.Unlock()
+	state.screen.Show()
+}
+
 func main() {
 	filename := flag.String("f", "", "Name of file to view.")
 	flag.Parse()
@@ -83,9 +211,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	conf := binxConfig{filename: *filename}
-
-	dat, err := ioutil.ReadFile(conf.filename)
+	dat, err := ioutil.ReadFile(*filename)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
@@ -94,104 +220,104 @@ func main() {
 
 	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
 	s, err := tcell.NewScreen()
-
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-
-	conf.screen = s
-	conf.screen.Init()
+	s.Init()
 	_, termHeight := s.Size()
-	conf.byteVisWidth = 80
-	conf.byteVisHeight = termHeight - 1
-	conf.byteStyle = tcell.StyleDefault.
-		Foreground(tcell.ColorLightBlue).
-		Background(tcell.ColorBlack)
-	conf.statStyle = tcell.StyleDefault
-	conf.alertStyle = tcell.StyleDefault
-	conf.screen.SetStyle(conf.byteStyle)
+	state := AppState{
+		filename:      *filename,
+		dat:           dat,
+		screen:        s,
+		byteVisWidth:  80,
+		byteVisHeight: termHeight - 1,
+		byteStyle: tcell.StyleDefault.
+			Foreground(tcell.ColorLightBlue).
+			Background(tcell.ColorBlack),
+		statStyle:  tcell.StyleDefault,
+		alertStyle: tcell.StyleDefault,
+		startByte:  0,
+	}
 
-	conf.startByte = 0
+	store := CreateStore(rootReducer(&state))
+
+	// Start the goroutine to receive actions.
+	go store.Reduce()
+
+	// store.Dispatcher <- Action{name: BinxSetScreenStyle, value: state.byteStyle}
 
 	for {
-		_, termHeight = conf.screen.Size()
-		w := conf.byteVisWidth
-		h := conf.byteVisHeight
-		numVisibleBytes := w * h
-		for i, b := range dat[conf.startByte : conf.startByte+int64(numVisibleBytes)] {
-			conf.byteStyle = conf.byteStyle.Foreground(tcell.Color(b))
-
-			s.SetContent(i%w, i/w, tcell.RuneBoard, nil, conf.byteStyle)
-		}
-
-		emitStatBar(conf)
-
-		// Do we have the byte seeker text input open?
-		if conf.mode == SeekInputMode {
-			emitStr(conf.screen, 0, 0, conf.alertStyle, "SEEK INPUT MODE")
-			emitStr(conf.screen,
-				conf.byteVisWidth-10,
-				conf.byteVisHeight-1,
-				tcell.StyleDefault,
-				fmt.Sprintf("Jump to: %s", conf.userInput),
-			)
-		}
-		s.Show()
-
-		// Input handling.
 		ev := s.PollEvent()
-		switch ev := ev.(type) {
-		case *tcell.EventResize:
-			w, h = s.Size()
-			conf.byteVisHeight = h - 1
-			numVisibleBytes = conf.byteVisHeight * conf.byteVisWidth
-			s.Sync()
-		case *tcell.EventKey:
-			if conf.mode == PatternInputMode {
-				if ev.Key() == tcell.KeyEscape {
-					conf.mode = NormalMode
-				} else if ev.Key() == tcell.KeyEnter {
-					conf.highlightPos, err = findBytePattern(conf.userInput, dat)
-					emitStr(conf.screen, 0, 10, tcell.StyleDefault, fmt.Sprintf("%d", conf.highlightPos))
-					conf.userInput = ""
-				} else {
-					conf.userInput = string(ev.Rune())
-				}
-			}
-			if conf.mode == SeekInputMode { // Text input mode
-				if ev.Key() == tcell.KeyEscape {
-					conf.mode = NormalMode
-				} else if ev.Key() == tcell.KeyEnter {
-					conf.startByte, err = strconv.ParseInt(conf.userInput, 0, 64) // hex, dec, or octal
-					conf.userInput = ""
-					conf.mode = NormalMode
-					if err != nil {
-						break
-					}
-				} else {
-					conf.userInput += string(ev.Rune())
-				}
-			} else {
-				// Mouse and arrow key browse mode, aka normal mode.
-				if ev.Key() == tcell.KeyEscape {
-					s.Fini()
-					os.Exit(0)
-				} else if ev.Key() == tcell.KeyDown {
-					conf.startByte += int64(w)
-					s.Sync()
-				} else if ev.Key() == tcell.KeyUp {
-					conf.startByte -= int64(w)
-					if conf.startByte < 0 {
-						conf.startByte = 0
-					}
-					s.Sync()
-				} else if ev.Rune() == 's' {
-					conf.mode = SeekInputMode
-				} else if ev.Rune() == 'f' {
-					conf.mode = PatternInputMode
-				}
-			}
-		}
+		HandleTcellEvent(store, ev)
+		// s.Show()
+		render(&state)
+
+		// // Do we have the byte seeker text input open?
+		// if conf.mode == SeekInputMode {
+		// 	emitStr(conf.screen, 0, 0, conf.alertStyle, "SEEK INPUT MODE")
+		// 	emitStr(conf.screen,
+		// 		conf.byteVisWidth-10,
+		// 		conf.byteVisHeight-1,
+		// 		tcell.StyleDefault,
+		// 		fmt.Sprintf("Jump to: %s", conf.userInput),
+		// 	)
+		// }
+		// Input handling.
+		// w, h := s.Size()
+
+		// switch ev := ev.(type) {
+		// case *tcell.EventResize:
+		// 	w, h = s.Size()
+		// 	state.byteVisHeight = h - 1
+		// 	emitStr(state.screen, 0, 10, tcell.StyleDefault, fmt.Sprintf("RESIGHS"))
+		// 	// numVisibleBytes := state.byteVisHeight * state.byteVisWidth
+		// 	s.Sync()
+		// case *tcell.EventKey:
+		// 	if state.mode == PatternInputMode {
+		// 		if ev.Key() == tcell.KeyEscape {
+		// 			state.mode = NormalMode
+		// 		} else if ev.Key() == tcell.KeyEnter {
+		// 			state.highlightPos, err = findBytePattern(state.userInput, dat)
+		// 			emitStr(state.screen, 0, 10, tcell.StyleDefault, fmt.Sprintf("%d", state.highlightPos))
+		// 			state.userInput = ""
+		// 		} else {
+		// 			state.userInput = string(ev.Rune())
+		// 		}
+		// 	}
+		// 	if state.mode == SeekInputMode { // Text input mode
+		// 		if ev.Key() == tcell.KeyEscape {
+		// 			state.mode = NormalMode
+		// 		} else if ev.Key() == tcell.KeyEnter {
+		// 			state.startByte, err = strconv.ParseInt(state.userInput, 0, 64) // hex, dec, or octal
+		// 			state.userInput = ""
+		// 			state.mode = NormalMode
+		// 			if err != nil {
+		// 				break
+		// 			}
+		// 		} else {
+		// 			state.userInput += string(ev.Rune())
+		// 		}
+		// 	} else {
+		// 		// Mouse and arrow key browse mode, aka normal mode.
+		// 		if ev.Key() == tcell.KeyEscape {
+		// 			s.Fini()
+		// 			os.Exit(0)
+		// 		} else if ev.Key() == tcell.KeyDown {
+		// 			state.startByte += int64(w)
+		// 			s.Sync()
+		// 		} else if ev.Key() == tcell.KeyUp {
+		// 			state.startByte -= int64(w)
+		// 			if state.startByte < 0 {
+		// 				state.startByte = 0
+		// 			}
+		// 			s.Sync()
+		// 		} else if ev.Rune() == 's' {
+		// 			state.mode = SeekInputMode
+		// 		} else if ev.Rune() == 'f' {
+		// 			state.mode = PatternInputMode
+		// 		}
+		// 	}
+		// }
 	}
 }
